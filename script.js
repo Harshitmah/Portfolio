21a6b6b00a754e5ccde4b0d39b4103d6384af87d
Harshitmah <harshitmah99@gmail.com>
2022-07-27 18:21:35 +0530

    hover added

--- script.js
+++ script.js
@@ -65,3 +65,22 @@ var tablinks = document.getElementsByClassName("tab-links");
             countEl.innerHTML = res.value;
           });
       }
+
+
+
+
+// ------------hover effect-----------------
+
+
+      const elements = document.getElementsByClassName('alpha');
+
+  for (let i = 0; i<=elements.length; i++)
+  {
+      elements[i].addEventListener('animationend',function(e){
+          elements[i].classList.remove('animated');
+      });
+
+      elements[i].addEventListener('mouseover',function(e){
+          elements[i].classList.add('animated')
+      })
+  }
